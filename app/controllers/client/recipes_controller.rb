fe77fe61c64f8ae1a953143f95e55f637093ccdd
Steve Han <shan61489@gmail.com>
2018-06-10 14:41:17 -0500

    added css components to frontend

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -1,61 +1,81 @@
 class Client::RecipesController < ApplicationController
   def index
-    response = Unirest.get("http://localhost:3000/api/recipes")
+    client_params = {
+                     search: params[:search]
+                    }
+
+    response = Unirest.get(
+                           "http://localhost:3000/api/recipes",
+                           parameters: client_params
+                           )
+
     @recipes = response.body
     render 'index.html.erb'
   end
+
   def new
     render 'new.html.erb'
   end
+
   def create
     client_params = {
                     title: params[:title],
                     chef: params[:chef],
                     ingredients: params[:ingredients],
                     directions: params[:directions],
-                    prep_time: params[:prep_time]
+                    prep_time: params[:prep_time],
+                    image_url: params[:image_url]
                     }
-    response = Unirest.post(
-                            "http://localhost:3000/api/recipes",
-                            parameters: client_params
-                            )
-    recipe = response.body
 
-    redirect_to "/client/recipes/#{recipe["id"]}"
+    response = Unirest.post(
+                           "http://localhost:3000/api/recipes",
+                           parameters: client_params
+                          )
+
+    recipe = response.body
+    flash[:success] = "Successfully created recipe"
+    redirect_to "/client/recipes/#{ recipe["id"] }"
   end
+
   def show
     recipe_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
     @recipe = response.body
     render 'show.html.erb'
   end
+
   def edit
-    @recipe_id = params[:id]
+    recipe_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
     @recipe = response.body
     render 'edit.html.erb'
   end
+
   def update
     client_params = {
-                        title: params[:title],
-                        chef: params[:chef],
-                        ingredients: params[:ingredients],
-                        directions: params[:directions],
-                        prep_time: params[:prep_time],
-                        image_url: params[:image_url]
-    }
+                    title: params[:title],
+                    chef: params[:chef],
+                    ingredients: params[:ingredients],
+                    directions: params[:directions],
+                    prep_time: params[:prep_time],
+                    image_url: params[:image_url]
+                    }
+
     response = Unirest.patch(
-                              "http://localhost:3000/api/recipes/#{params[:id]}",
-                              parameters: client_params)
+                             "http://localhost:3000/api/recipes/#{ params[:id] }",
+                             parameters: client_params
+                            )
     recipe = response.body
     flash[:success] = "Successfully Updated Recipe"
-    redirect_to "/client/recipes/#{recipe["id"]}"
+    redirect_to "/client/recipes/#{ recipe["id"] }"
   end
+
   def destroy
     recipe_id = params[:id]
-    response = Unirest.delete("http://localhost:3000/api/recipes/#{recipe_id}")
+    response = Unirest.delete("http://localhost:3000/api/recipes/#{ recipe_id }")
     
-    redirect_to "/" #sending it to the index
+    flash[:success] = "Successfully destroyed recipe"
+    redirect_to "/"
   end
 
 end
