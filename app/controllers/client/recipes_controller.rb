6feaeefdc10571086eea72a3e5fe8e81bda59e37
Steve Han <shan61489@gmail.com>
2019-02-02 09:28:16 -0600

    changes to css and index.erb

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -20,7 +20,6 @@ class Client::RecipesController < ApplicationController
   def create
     client_params = {
                     title: params[:title],
-                    chef: params[:chef],
                     ingredients: params[:ingredients],
                     directions: params[:directions],
                     prep_time: params[:prep_time],
@@ -54,7 +53,6 @@ class Client::RecipesController < ApplicationController
   def update
     client_params = {
                     title: params[:title],
-                    chef: params[:chef],
                     ingredients: params[:ingredients],
                     directions: params[:directions],
                     prep_time: params[:prep_time],
@@ -77,5 +75,4 @@ class Client::RecipesController < ApplicationController
     flash[:success] = "Successfully destroyed recipe"
     redirect_to "/"
   end
-
 end
